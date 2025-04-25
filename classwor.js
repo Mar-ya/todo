87bf559297b8c209433572e305dcecceb4da0598
unknown <aramidealarape02@gmail.com>
2025-04-25 14:23:21 +0100

    this is todo

---
+++ classwor.js
@@ -0,0 +1,106 @@
+// // const a=4 
+// // // var changes but const is constant always use const
+// // const b=7
+// // const firstname= "maryam snausi doctor nurse"
+// // const you= "false"
+
+// // console.log(a**b)
+// // console.log(a+  firstname)
+// // console.log(firstname.length)
+// // console.log(firstname.charAt(0).toUpperCase().concat(firstname.slice(1,firstname.length)))
+// // // console.log(firstname.charAt(3))
+// // // console.log(firstname.trim)
+// // // console.log(firstname.concat(you))
+// // //  console.log( firstname.toLocaleLowerCase)
+// // //  console.log(firstname.split(""))
+// // //  console.log(firstname.slice(0,5))
+ 
+// //  function formatnumbers(params) {
+// //   if(typeof params ==='string'){
+// //   // return params.slice(0,3) +"-" + params.slice(3,7) + "-" + params.slice(7)
+// //   }
+// //   return params.slice(0,3).concat("-").concat(params.slice(3,7)).concat("-").concat(params.slice(7))}
+// //  const fullname ="Sanusi Maryam" 
+// //  const alias ="SM"
+// //  function toAlias(params){
+// //   if( typeof params =='string'){
+// //    const edit =params.split(" ")
+// //    console.log(edit)
+// //    return edit[0].charAt(0).concat (edit[1].charAt(0))
+// //   }
+  
+// //  }
+// //  console.log(toAlias(fullname))
+// //   // console.log(formatnumbers ('08135289834'))
+  
+  
+// //  // const array =[1,2,3,4,5,"mydid" , false,true,]
+ 
+// //  // console.log(array[4])
+ 
+// //  const products =[ 2,3,4,5,6]
+// //  products.push(19)
+// //  console.log(products)
+ 
+// //  object
+// //  const obj ={
+// //   gender: "female",
+// //   age: 30,
+// //   iscomplexion:false
+// //  }
+// //  // const ab =obj.age        turn it to array
+// //  const bb =obj["age"]
+ 
+
+// // properties or method of Boolean
+// // 1. to String
+
+
+// // properties of array
+// // 1. length
+// // 2. join
+// // 3. properties4. push
+// // 5. shift
+// scroll
+// ResizeObserverSize
+// copywithin
+// sort
+// index
+// includes
+// flatmap
+// reduce
+// findindex
+// flatmap
+// random
+// at
+// copywithin
+// reverse
+// splice
+// // unshift
+
+ // const arrnumbers =[1,2,,3,4,5]
+ // const arraystrings =["a","b","c","d"]
+ // const arraybool =[true,false,true,false]
+ []
+ // const arraysobj =[
+ //  {
+ //   name:sanusi maryam, 
+ //   age: 1,
+ //   gender: "male" ,
+ //   istutor: true,
+ //   isstudent: false
+   
+
+
+ const emptyarray =[]
+ emptyarray.push("maryam",56)
+ console.log(emptyarray)
+ console.log(emptyarray.length)
+ 
+ const arraynumbers =[45,9,78,96,65,42]
+ console.log(arraynumbers)
+ 
+ const arrayslice
+ 
+ 
+ 
---
+++ statments.js
@@ -0,0 +1,68 @@
+// == equal value but different data typeof
+// === equal value and equal datat typeof
+// != is not equal to in equal value but different data typeof
+// !== is not equal to in equal value and equal data typeof
+
+
+const age = 19
+if ( age >= 18) {
+ console.log("i am 18")
+}
+else if( age >=1){
+ console.log( "I am less than 18 years ols")
+}
+else{
+ console.log ("i am negative age")
+}
+const foodtype ="salaad"
+if( foodtype =="garri") {
+ console.log("you drink gaari and you need epa")
+}
+else if (foodtype =="beans") {
+ console.log("you eat beans")
+}
+else if (foodtype=="bread"){
+ const array =[]
+}
+
+else if (foodtype =="yam"){
+ const array =[1,2,3,4]
+ console.log ("you eat bread")
+}
+
+else{
+ console.log("you cannot find the food you are requesting")
+}
+
+var retry =0
+ // ++ only increase by 1
+retry +1
+retry +1
+retry +1
+console.log(retry)
+if (retry >=3){
+ console.log("you are log oout and banned")
+}
+else if (retry >1 && retry <3){
+ console.log
+}
+console.log(retry)
+const islogged =retry >=3 ? "you can log in ": retry ==1?" you have 2 more trial": retry ==2 ? "you have one more trial": "you are banned"
+
+
+
+
+switch (typeoffood) {
+case "amala":
+console.log("you eat amala")  
+
+
+ 
+break;
+case "beans" :
+ break ;
+ default :
+ break;}
+ 
+ 
+ function HandLogin(isLogged  )
